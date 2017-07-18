12b5cbbe08f24b23a6d119e3d93754e66f4178ab
An0num0us <p.kocia@wp.pl>
2017-07-18 22:36:58 +0200

    New error types

--- Errors.js
+++ Errors.js
@@ -1,26 +1,42 @@
+module.exports = {
+    DatabaseError: DatabaseError,
+    NullFieldError: NullFieldError,
+    RequestError: RequestError,
+    AuthorizationError: AuthorizationError,
+    RequiredArgumentNotSuppliedError: RequiredArgumentNotSuppliedError
+};
+
 class DatabaseError extends Error {
     constructor(message) {
         super(message);
-        this.errorType = 'Database Error';
+        this.errorType = 'Database';
+    }
+}
+
+class NullFieldError extends DatabaseError {
+    constructor(fieldName) {
+        super('Field ' + fieldName + 'was declared as "not null" but null was given');
+        this.errorType = 'NullField';
     }
 }
 
 class RequestError extends Error {
     constructor(message) {
         super(message);
-        this.errorType = 'Request Error';
+        this.errorType = 'Request';
     }
 };
 
 class AuthorizationError extends RequestError {
     constructor(message) {
         super(message);
-        this.errorType = 'Authorization Error';
+        this.errorType = 'Authorization';
     }
 };
 
-module.exports = {
-    DatabaseError: DatabaseError,
-    RequestError: RequestError,
-    AuthorizationError: AuthorizationError
-};
+class RequiredArgumentNotSuppliedError extends Error {
+    constructor(argumentName) {
+        super('The required field ' + argumentName + ' was not supplied');
+        this.errorType = 'RequiredArgumentNotSupplied';
+    }
+}

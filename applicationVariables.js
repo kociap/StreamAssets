f11f9f58ed7ffbd936513c0b8b4dc8a74154b723
An0num0us <p.kocia@wp.pl>
2017-07-25 14:04:50 +0200

    Reduced expiration time of account cookie

--- applicationVariables.js
+++ applicationVariables.js
@@ -7,7 +7,7 @@ module.exports = {
     DOMAIN_NO_PROTOCOL: 'localhost:3000',
     
     USER_ACCOUNT_COOKIE_NAME: 'UTK-A',
-    USER_ACCOUNT_COOKIE_MAX_AGE: 1000 * 3600 * 24 * 7,
+    USER_ACCOUNT_COOKIE_MAX_AGE: 1000 * 3600 * 24 * 2,
 
     YOUTUBE_OAUTH_REDIRECT_URI: 'http://localhost:3000/youtube/auth',
     YOUTUBE_API_SCOPE: 'https://www.googleapis.com/auth/youtube',

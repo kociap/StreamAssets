5e09c241a611f2924b2e1fc0bdc6e31870e856e0
An0num0us <p.kocia@wp.pl>
2017-07-25 14:05:57 +0200

    Removed unused parts and reorganized the file

--- index.js
+++ index.js
@@ -18,40 +18,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 require('./Router.js').setRouter(app);
+const ApplicationVariables = require('./ApplicationVariables.js');
 
 // Request mapping
 require('./RequestMapping/RequestMapper.js');
 require('./RequestMapping/Sessions.js');
 require('./RequestMapping/UserAuthentication.js');
-require('./RequestMapping/Login.js');
 
-const ApplicationVariables = require('./ApplicationVariables.js');
 const RoomsController = require('./RoomsController.js');
-
-// Data refresh time in milliseconds
-const REFRESH_TIMEOUT = 500;
-const ACCOUNT_ID = 'UCtlzktCIv86iJ3fFWlkvVog';
+RoomsController.init(io);
 
 http.listen(Number(ApplicationVariables.SERVER_PORT), () => {
     console.log(`Server launched at ${ApplicationVariables.SERVER_PORT}`);
 });
-
-RoomsController.init(io);
-
-// /**
-//  * Loads tokens asynchronously
-//  * @param {string} user 
-//  * @param {string} scope 
-//  * @returns {Promise<object>}
-//  */
-// function loadTokens(user, scope) {
-//     return new Promise((resolve, reject) => {
-//         fs.readFile('tokens.txt', (error, data) => {
-//             if(error || data || !JSON.parse(data).hasOwnProperty(scope)) {
-//                 resolve({});
-//             } else {
-//                 resolve(JSON.parse(data)[scope]);
-//             }
-//         });
-//     });
-// }
